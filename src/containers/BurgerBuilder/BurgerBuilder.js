c475ddde3efafb7f6b2de12ffbe712fc0afe0924
Natalie Groeneboom <natalie@digidrop.io>
2019-07-04 17:30:54 +0100

    order button functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,8 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
     }
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
