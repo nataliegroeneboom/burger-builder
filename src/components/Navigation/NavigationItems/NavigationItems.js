e858e751ce29aff35978e5316d06657df187a614
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-08-10 13:16:43 +0100

    implementing navigation links 256

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
-import {Link} from 'react-router-dom';
+
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-       <Link to="/" exact><NavigationItem link="/" active>Burger Builder</NavigationItem>   </Link> 
-      <Link to="/checkout"><NavigationItem link="/">Checkout</NavigationItem></Link>
+       <NavigationItem link="/">Burger Builder</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
 
