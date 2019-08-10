61246ccc9e1f95016083263454e0ffd82aefaef1
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-08-10 11:54:59 +0100

    adding orders page 255

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Order from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <Layout>
             <Switch>
                 <Route path="/" exact component={BurgerBuilder} />
+                <Route path="/orders" component={Orders} />
                 <Route path="/checkout" component={Checkout} />
             </Switch>
           </Layout>  
---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Order.module.css';
+
+const order = (props) => (
+    <div className={styles.Order}>
+        <p>Ingredients: Salad (quantity)</p>
+     </div>   
+)
+
+export default order;
